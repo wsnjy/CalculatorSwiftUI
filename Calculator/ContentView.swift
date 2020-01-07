1a2cf9e83d44f460eda690e5a1d98d0259aea9b1
wisnu <wisnus@linkaja.io>
2020-01-07 13:41:10 +0700

    master spacing eps 1

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -9,9 +9,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let buttons = [["7", "8", "9", "X"],
+                   ["4", "5", "6", "X"],
+                   ["1", "2", "3", "X"],
+                   ["0", ".", ".", "X"]
+    ]
+    
     var body: some View {
-        Text("Hello, World!")
+        
+        ZStack(alignment: .bottom){
+            
+            Color.black.edgesIgnoringSafeArea(.all)
+            
+            VStack(spacing: 12){
+                HStack {
+                    Spacer()
+                    Text("42")
+                        .font(.system(size: 64))
+                        . foregroundColor(.white)
+                }.padding()
+                
+                ForEach(buttons, id: \.self) { row in
+                    HStack(spacing: 12){
+                        ForEach(row, id: \.self) { button in
+                            Text(button)
+                                .font(.system(size: 32))
+                                .frame(width: self.buttonWidth(), height: self.buttonWidth())
+                                . foregroundColor(.white)
+                                .background(Color.yellow)
+                                .cornerRadius(self.buttonWidth())
+                        }
+                    }
+                }
+            }.padding(.bottom)
+        }
     }
+    
+    func buttonWidth() -> CGFloat {
+        return (UIScreen.main.bounds.width - 5 * 12) / 4
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
